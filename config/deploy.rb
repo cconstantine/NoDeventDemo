74b5798eaaf6a9e2c66876f1b416e3a2cdb1d25f
Chris Constantine <chris@omadahealth.com>
2012-05-20 14:09:31 -0700

    more deploy
    Signed-off-by: Chris Constantine <chris@omadahealth.com>

--- config/deploy.rb
+++ config/deploy.rb
@@ -9,14 +9,17 @@ set :git_shallow_clone, 1
 set :deploy_to, "/var/www/nodevent"
 default_run_options[:pty] = true
 
+set :rvm_ruby_string, '1.9.2-p318@nodevent'
+set :rvm_install_ruby, :install
+require "rvm/capistrano"
 
 #Bundler
 set :bundle_dir,          fetch(:shared_path)+"/bundle"
 set :bundle_flags,       "--deployment --quiet"
 
 #Servers
-role :app, "nodevent.com"
-role :db, "nodevent.com", :primary => true
+role :app, "ec2-75-101-164-22.compute-1.amazonaws.com"
+role :db,  "ec2-75-101-164-22.compute-1.amazonaws.com", :primary => true
 
 
 before "deploy:symlink", "deploy:migrate"
